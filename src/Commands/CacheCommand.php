b6fa79f663fa34ee80e13ea605cce6baad8e5a8c
Yann Choquet <yann.choquet@efipeek.com>
2016-10-13 22:05:14 +0200

    Add Media:clearCache and Cache...flush

--- src/Commands/CacheCommand.php
+++ src/Commands/CacheCommand.php
@@ -19,6 +19,7 @@ class CacheCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         \Tools::clearSmartyCache();
-        \CacheFs::deleteCacheDirectory();
+        \Cache::getInstance()->flush();
+        \Media::clearCache();
     }
 }
